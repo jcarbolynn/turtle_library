ead18a9ac104defd5265f694b781d539c0035c98
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-03 13:56:31 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,51 @@
 import turtle as t
+import random
 from turtle import Screen
 
 tim = t.Turtle()
 
-########### Challenge 2 - Draw a Dashed Line ########
-tim.penup()
-tim.setpos(-150,-150)
-tim.setheading(45)
+########### Challenge 3 - Draw Shapes ########
 
-# tim.pendown()
-# for _ in range(15):
-#   tim.pencolor("black")
-#   tim.forward(10)
-  
-#   tim.pencolor("white")
-#   tim.forward(10)
+def draw_shape(num_sides):
+  angle = 360/num_sides
+  for _ in range(num_sides):
+    tim.forward(50)
+    tim.right(angle)
 
-for _ in range(15):
-  tim.pendown()
-  tim.forward(10)
-  
-  tim.penup()
-  tim.forward(10)
+colors = ["cornflower blue",
+         "deep sky blue",
+         "pale green",
+         "medium aquamarine",
+         "wheat",
+         "peach puff",
+         "light sky blue"]
+
+for shape_side_n in range(3,11):
+  tim.color(random.choice(colors))
+  draw_shape(shape_side_n)
+
+
+'''
+# color = random.choices(range(256), k=3)
+# tim.pencolor(color[0], color[1], color[2])
+
+# r = random.randint(0, 255)
+# g = random.randint(0, 255)
+# b = random.randint(0, 255)
+# random_color = (r, g, b)
+# tim.pencolor(random_color)
 
 screen = Screen()
-screen.exitonclick()
+screen.colormode(255)
 
+for sides in range(3,11):
+  r = random.randint(0, 255)
+  g = random.randint(0, 255)
+  b = random.randint(0, 255)
+  tim.pencolor(r, g, b)
+  
+  for _ in range(sides):
+    tim.forward(40)
+    tim.right(360/sides)
+
+'''
