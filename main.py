315239ac1fad03a04b2f0f58cb5d5e6ca20f83be
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-03 13:58:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,42 @@
 import turtle as t
-from turtle import Screen 
 import random
 
 tim = t.Turtle()
+t.colormode(255)
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
 
-########### Challenge 4 - Random Walk ########
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+########### Challenge 5 - Spirograph ########
 
-#thicker lines
-#speed up turtle
-
-screen = Screen()
-# screen.onclick(tim.goto)
-
-def isInScreen(wn,t):
-  '''returns true if turtle still in screen, false if out of screen'''
-  leftBound = -(wn.window_width() / 2)
-  rightBound = wn.window_width() / 2
-  topBound = wn.window_height() / 2
-  bottomBound = -(wn.window_height() / 2)
-
-  turtleX = t.xcor()
-  turtleY = t.ycor()
-
-  stillIn = True
-  if turtleX > rightBound or turtleX < leftBound:
-      stillIn = False
-  if turtleY > topBound or turtleY < bottomBound:
-      stillIn = False
-
-  return stillIn
-
-
-
-tim.hideturtle()
-tim.pensize(10)
 tim.speed(0)
+tim.hideturtle()
 
-# def stop():
-#   screen.exitonclick()
+def draw_spirograph(size_of_gap):
+  for _ in range(int(360/ size_of_gap)):
+    tim.color(random_color())
+    tim.circle(50)
+    tim.setheading(tim.heading() + size_of_gap)
 
+draw_spirograph(15)
 
+# only draw as many circles as we need
+# for _ in range(100):
+#   tim.color(random_color())
+#   tim.circle(50)
+#   tim.setheading(tim.heading() + 10)
 
-#want it to continue until i click screen
-for _ in range(0,500):
-  # program stops when runs off screen
-  # instead change directions so stays on screen
-  while isInScreen(screen, tim):
-    tim.color(random.choice(colors))
-    tim.forward(random.randint(1, 50))
-    tim.right(random.randint(1, 5)*90)
+# for i in range(36):
+#   tim.color(random_color())
+#   tim.circle(50)
+#   tim.left(10)
 
+  
+# for num in range(1,361):
+#   if num%3 == 0:
+#     tim.color(random_color())
+#     tim.circle(50, num)
 
-
-# def f():
-#     tim.fd(50)
-
-# screen.onkey(f, "Up")
-# screen.listen()
